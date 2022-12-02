1e67db777500ec2121f25fcaf44542d4b13685e6
Max Wilson <maxjw98@gmail.com>
2022-12-02 10:45:57 +0000

    Updated links and organisation

--- src/components/Movie.svelte
+++ src/components/Movie.svelte
@@ -1,320 +1,323 @@
-<script>
-	import dayjs from 'dayjs';
-	import { onMount } from 'svelte';
-	import duration from 'dayjs/plugin/duration.js';
-	import relativeTime from 'dayjs/plugin/relativeTime';
+<script lang="ts">
+    import dayjs from 'dayjs';
+    import { onMount } from 'svelte';
+    import duration from 'dayjs/plugin/duration.js';
+    import relativeTime from 'dayjs/plugin/relativeTime';
 
-	export let title;
-	export let date;
-	export let length;
-	export let poster;
-	export let tv;
-	export let disneyplus;
+    export let title;
+    export let date;
+    export let length;
+    export let poster;
+    export let tv;
+    export let disneyplus;
 
-	dayjs.extend(duration);
-	dayjs.extend(relativeTime);
+    dayjs.extend(duration);
+    dayjs.extend(relativeTime);
 
-	let w;
-	let posterWidth;
-	let r, local, timer;
-	let diff = 0;
-	let dateFormat = 'YYYY-MM-DD[T]HH:mm:ss[Z]';
-	let target = dayjs(date, dateFormat);
-	let done = false;
-	let remaining = 'Loading...';
+    let r, local, timer, w, posterWidth;
+    let diff: number = 0;
+    let dateFormat: string = 'YYYY-MM-DD[T]HH:mm:ss[Z]';
+    let target = dayjs(date, dateFormat);
+    let done: boolean = false;
+    let remaining: string | any = 'Loading...';
 
-	function getRemaining() {
-		if (diff > 0) {
-			r = dayjs.duration(diff);
-			let week = Math.floor(r.days() / 7);
-			let day = r.days() - week * 7;
-			remaining = {
-				years: r.years(),
-				months: r.months(),
-				weeks: week,
-				days: day,
-				hours: r.hours(),
-				minutes: r.minutes(),
-				seconds: r.seconds(),
-			};
-			diff -= 1000;
-		} else {
-			done = true;
-			clearInterval(timer);
-		}
-	}
+    function getRemaining() {
+        if (diff > 0) {
+            r = dayjs.duration(diff);
+            let week = Math.floor(r.days() / 7);
+            let day = r.days() - week * 7;
+            remaining = {
+                years: r.years(),
+                months: r.months(),
+                weeks: week,
+                days: day,
+                hours: r.hours(),
+                minutes: r.minutes(),
+                seconds: r.seconds(),
+            };
+            diff -= 1000;
+        } else {
+            done = true;
+            clearInterval(timer);
+        }
+    }
 
-	if (dayjs.isDayjs(target)) {
-		local = dayjs();
-		diff = target.valueOf() - local.valueOf();
-	}
+    if (dayjs.isDayjs(target)) {
+        local = dayjs();
+        diff = target.valueOf() - local.valueOf();
+    }
 
-	onMount(async () => {
-		getRemaining();
-		timer = setInterval(getRemaining, 1000);
-	});
+    onMount(async () => {
+        getRemaining();
+        timer = setInterval(getRemaining, 1000);
+    });
 </script>
 
 <svelte:window bind:innerWidth={w} />
 
 <div class="tile">
-	<div class="poster-container" bind:clientWidth={posterWidth}>
-		<a href={disneyplus ? disneyplus : '#/'}>
-			<img src={poster} alt={title} class="poster" />
-		</a>
-		{#if disneyplus}
-			<div class="overlay">
-				<i class="fa-solid fa-circle-play fa-4x" />
-			</div>
-		{/if}
-	</div>
-	<div
-		class="details"
-		style="padding-left: calc({posterWidth}px + var(--additional-padding))"
-	>
-		<h2 class="please">{title}</h2>
-		<h4>{dayjs(date).format('MMMM D, YYYY')}</h4>
-		<div class="blocks">
-			<p class="film-length">
-				{length != '0' ? length + (tv ? ' episodes' : '') : 'Unknown'}
-			</p>
-		</div>
-		{#if done === false}
-			<p class="countdown">
-				{#if remaining.years > 0}
-					{remaining.years + ' years'},
-					{remaining.months + ' months'},
-					{remaining.weeks + ' weeks'}
-				{:else if remaining.months > 0}
-					{remaining.months + ' months'},
-					{remaining.weeks + ' weeks'},
-					{remaining.days + ' days'}
-				{:else if remaining.weeks > 0}
-					{remaining.weeks + ' weeks'},
-					{remaining.days + ' days'},
-					{remaining.hours + ' hours'}
-				{:else if remaining.days > 0}
-					{remaining.days + ' days'},
-					{remaining.hours + ' hours'},
-					{remaining.minutes + ' minutes'}
-				{:else if remaining.hours > 0}
-					{remaining.hours + ' hours'},
-					{remaining.minutes + ' minutes'},
-					{remaining.seconds + ' seconds'}
-				{:else if remaining.minutes > 0}
-					{remaining.minutes + ' minutes'},
-					{remaining.seconds + ' seconds'}
-				{:else if remaining.seconds > 0}
-					{remaining.seconds + ' seconds'}
-				{/if}
-			</p>
-		{:else}
-			<p>Out now!</p>
-		{/if}
-	</div>
+    <div class="poster-container" bind:clientWidth={posterWidth}>
+        <a href={disneyplus ? disneyplus : '#/'}>
+            <img src={poster} alt={title} class="poster" />
+        </a>
+        {#if disneyplus}
+            <div class="overlay">
+                <i class="fa-solid fa-circle-play fa-4x" />
+            </div>
+        {/if}
+    </div>
+    <div class="details" style="padding-left: calc({posterWidth}px + var(--additional-padding))">
+        <h2>{title}</h2>
+        <h4>{dayjs(date).format('MMMM D, YYYY')}</h4>
+        <div class="blocks">
+            <p class="film-length">
+                {length != '0' ? length + (tv ? ' episodes' : '') : 'Unknown'}
+            </p>
+        </div>
+        {#if done === false}
+            <p class="countdown">
+                {#if remaining.years > 0}
+                    {remaining.years + ' years'},
+                    {remaining.months + ' months'},
+                    {remaining.weeks + ' weeks'}
+                {:else if remaining.months > 0}
+                    {remaining.months + ' months'},
+                    {remaining.weeks + ' weeks'},
+                    {remaining.days + ' days'}
+                {:else if remaining.weeks > 0}
+                    {remaining.weeks + ' weeks'},
+                    {remaining.days + ' days'},
+                    {remaining.hours + ' hours'}
+                {:else if remaining.days > 0}
+                    {remaining.days + ' days'},
+                    {remaining.hours + ' hours'},
+                    {remaining.minutes + ' minutes'}
+                {:else if remaining.hours > 0}
+                    {remaining.hours + ' hours'},
+                    {remaining.minutes + ' minutes'},
+                    {remaining.seconds + ' seconds'}
+                {:else if remaining.minutes > 0}
+                    {remaining.minutes + ' minutes'},
+                    {remaining.seconds + ' seconds'}
+                {:else if remaining.seconds > 0}
+                    {remaining.seconds + ' seconds'}
+                {/if}
+            </p>
+        {:else}
+            <p>Out now!</p>
+        {/if}
+    </div>
 </div>
 
 <style>
-	.tile {
-		height: 200px;
-		width: 100%;
-		margin-bottom: 40px;
-		position: relative;
-		transition: all 0.2s;
-		border-radius: 10px;
-	}
+    .tile {
+        height: 200px;
+        width: 100%;
+        margin-bottom: 40px;
+        position: relative;
+        transition: all 0.2s;
+        border-radius: 10px;
+    }
 
-	.details {
-		width: 100%;
-		height: 80%;
-		position: absolute;
-		bottom: 0;
-		padding: 3px;
-		border-radius: 10px;
-		box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.25);
-		box-sizing: border-box;
-		margin-left: 1px; /* Removes pixel corner in poster */
-		background: var(--background);
-		--additional-padding: 10px;
-		overflow: hidden;
-	}
+    .details {
+        width: 100%;
+        height: 80%;
+        position: absolute;
+        bottom: 0;
+        padding: 3px;
+        border-radius: 10px;
+        box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.25);
+        box-sizing: border-box;
+        margin-left: 1px; /* Removes pixel corner in poster */
+        background: var(--background);
+        --additional-padding: 10px;
+        overflow: hidden;
 
-	.details h2,
-	.details h4,
-	.details p {
-		margin-block-start: 0;
-		margin-block-end: 0;
-		margin-inline-start: 0;
-		margin-inline-end: 0;
-	}
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
 
-	.poster-container {
-		height: 100%;
-		width: auto;
-		max-width: 32vw;
-		position: absolute;
-		bottom: 0;
-		z-index: 9999;
-		box-sizing: border-box;
-		border-radius: 10px;
-		box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.25);
-		overflow: hidden;
-		transform: translate3d(0, 0, 0);
-		-webkit-transform: translate3d(0, 0, 0);
-	}
+    .countdown {
+        margin-top: auto !important;
+    }
 
-	.poster {
-		height: 100%;
-		object-fit: cover;
-		display: block;
-		transition: all 0.4s ease;
-		-khtml-user-select: none;
-		-o-user-select: none;
-		-moz-user-select: none;
-		-webkit-user-select: none;
-		user-select: none;
-	}
+    .details h2,
+    .details h4,
+    .details p {
+        margin-block-start: 0;
+        margin-block-end: 0;
+        margin-inline-start: 0;
+        margin-inline-end: 0;
+    }
 
-	.poster-container:hover .poster {
-		transform: scale(1.2);
-	}
+    .poster-container {
+        height: 100%;
+        width: auto;
+        max-width: 32vw;
+        position: absolute;
+        bottom: 0;
+        z-index: 9999;
+        box-sizing: border-box;
+        border-radius: 10px;
+        box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.25);
+        overflow: hidden;
+        transform: translate3d(0, 0, 0);
+        -webkit-transform: translate3d(0, 0, 0);
+    }
 
-	.poster-container a {
-		z-index: 8;
-		color: black;
-	}
+    .poster {
+        height: 100%;
+        object-fit: cover;
+        display: block;
+        transition: all 0.4s ease;
+        -khtml-user-select: none;
+        -o-user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        user-select: none;
+    }
 
-	[href='#/'] {
-		pointer-events: none;
-		cursor: default;
-		text-decoration: none;
-	}
+    .poster-container:hover .poster {
+        transform: scale(1.2);
+    }
 
-	.overlay {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		pointer-events: none;
-		cursor: default;
-		text-decoration: none;
-		color: #dbdbdb;
-		opacity: 0;
-		background-color: rgba(0, 0, 0, 0.5);
-		transition: all 0.4s ease;
-	}
+    .poster-container a {
+        z-index: 8;
+        color: black;
+    }
 
-	.poster-container:hover .overlay {
-		opacity: 1;
-	}
+    [href='#/'] {
+        pointer-events: none;
+        cursor: default;
+        text-decoration: none;
+    }
 
-	.blocks {
-		margin-top: 5px !important;
-		margin-bottom: 5px !important;
-	}
+    .overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        pointer-events: none;
+        cursor: default;
+        text-decoration: none;
+        color: #dbdbdb;
+        opacity: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        transition: all 0.4s ease;
+    }
 
-	.film-length {
-		padding: 5px;
-		border: 1px solid var(--text-muted);
-		display: inline-block;
-		color: var(--text-main);
-		border-radius: 5px;
-		font-size: 0.9rem;
-	}
+    .poster-container:hover .overlay {
+        opacity: 1;
+    }
 
-	/* Desktop */
-	@media all and (min-width: 1024px) {
-		.tile {
-			height: 350px;
-		}
+    .blocks {
+        margin-top: 5px !important;
+        margin-bottom: 5px !important;
+    }
 
-		.blocks {
-			margin-top: 10px !important;
-			margin-bottom: 10px !important;
-		}
+    .film-length {
+        padding: 5px;
+        border: 1px solid var(--text-muted);
+        display: inline-block;
+        color: var(--text-main);
+        border-radius: 5px;
+        font-size: 0.9rem;
+    }
 
-		.details h4 {
-			font-size: 1.2em;
-		}
+    /* Desktop */
+    @media all and (min-width: 1024px) {
+        .tile {
+            height: 350px;
+        }
 
-		.details h2 {
-			font-size: 2em;
-		}
+        .blocks {
+            margin-top: 10px !important;
+            margin-bottom: 10px !important;
+        }
 
-		.details {
-			padding: 20px;
-			--additional-padding: 20px;
-		}
+        .details h4 {
+            font-size: 1.2em;
+        }
 
-		.poster-container {
-			max-width: 236.22px;
-		}
-	}
+        .details h2 {
+            font-size: 2em;
+        }
 
-	/* Tablet Landscape */
-	@media all and (min-width: 768px) and (max-width: 1024px) {
-		.tile {
-			height: 300px;
-		}
+        .details {
+            padding: 20px;
+            --additional-padding: 20px;
+        }
 
-		.details {
-			padding: 10px;
-		}
+        .poster-container {
+            max-width: 236.22px;
+        }
+    }
 
-		.poster-container {
-			max-width: 202.47px;
-		}
-	}
+    /* Tablet Landscape */
+    @media all and (min-width: 768px) and (max-width: 1024px) {
+        .tile {
+            height: 300px;
+        }
 
-	/* Tablet */
-	@media all and (min-width: 480px) and (max-width: 768px) {
-		.tile {
-			height: 250px;
-		}
+        .details {
+            padding: 10px;
+        }
 
-		.details {
-			padding: 10px;
-		}
+        .poster-container {
+            max-width: 202.47px;
+        }
+    }
 
-		.poster-container {
-			max-width: 168.72px;
-		}
-	}
+    /* Tablet */
+    @media all and (min-width: 480px) and (max-width: 768px) {
+        .tile {
+            height: 250px;
+        }
 
-	/* Phone landscape & smaller */
-	@media all and (max-width: 480px) {
-		.tile {
-			height: 200px;
-		}
+        .details {
+            padding: 10px;
+        }
 
-		.details h4 {
-			font-size: 0.8em;
-		}
+        .poster-container {
+            max-width: 168.72px;
+        }
+    }
 
-		.details h2 {
-			font-size: 1.2em;
-		}
+    /* Phone landscape & smaller */
+    @media all and (max-width: 480px) {
+        .tile {
+            height: 200px;
+        }
 
-		.details {
-			padding: 10px;
-		}
+        .details h4 {
+            font-size: 0.8em;
+        }
 
-		.poster-container {
-			max-width: 134.97px;
-		}
-	}
+        .details h2 {
+            font-size: 1.2em;
+        }
 
-	/* Phone landscape & smaller */
-	@media all and (max-width: 280px) {
-		.tile {
-			height: 150px;
-		}
+        .details {
+            padding: 10px;
+        }
 
-		.poster-container {
-			max-width: 101.22px;
-		}
-	}
+        .poster-container {
+            max-width: 134.97px;
+        }
+    }
+
+    /* Phone landscape & smaller */
+    @media all and (max-width: 280px) {
+        .tile {
+            height: 150px;
+        }
+
+        .poster-container {
+            max-width: 101.22px;
+        }
+    }
 </style>
--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,158 +1,155 @@
-<script>
-	import Movie from './Movie.svelte';
-	import { slide } from 'svelte/transition';
+<script lang="ts">
+    import Movie from './Movie.svelte';
+    import { slide } from 'svelte/transition';
 
-	let isOpen = false;
-	const toggle = () => (isOpen = !isOpen);
+    let isOpen = false;
+    const toggle = () => (isOpen = !isOpen);
 
-	function custom_sort(a, b) {
-		return (
-			new Date(a.ReleaseDate).getTime() -
-			new Date(b.ReleaseDate).getTime()
-		);
-	}
-	async function fetchReleases() {
-		const res = await fetch('/marvel.json');
-		const data = await res.json();
+    function custom_sort(a, b) {
+        return new Date(a.ReleaseDate).getTime() - new Date(b.ReleaseDate).getTime();
+    }
+    async function fetchReleases() {
+        const res = await fetch('/marvel.json');
+        const data = await res.json();
 
-		if (res.ok) {
-			data.sort(custom_sort);
-			let ToDate = new Date();
-			const [r_old, r_new] = data.reduce(
-				([r_old, r_new], item) => {
-					(new Date(item.ReleaseDate).getTime() < ToDate.getTime()
-						? r_old
-						: r_new
-					).push(item);
-					return [r_old, r_new];
-				},
-				[[], []]
-			);
-			return [r_old, r_new];
-		} else {
-			throw new Error(data);
-		}
-	}
+        if (res.ok) {
+            data.sort(custom_sort);
+            let ToDate = new Date();
+            const [r_old, r_new] = data.reduce(
+                ([r_old, r_new], item) => {
+                    (new Date(item.ReleaseDate).getTime() < ToDate.getTime() ? r_old : r_new).push(
+                        item,
+                    );
+                    return [r_old, r_new];
+                },
+                [[], []],
+            );
+            r_old.reverse();
+            return [r_old, r_new];
+        } else {
+            throw new Error(data);
+        }
+    }
 
-	let releases = fetchReleases();
+    let releases = fetchReleases();
 </script>
 
 {#await releases}
-	<div class="center">
-		<span class="loader" />
-	</div>
+    <div class="center">
+        <span class="loader" />
+    </div>
 {:then releases}
-	<button on:click={toggle} aria-expanded={isOpen} id="released_button"
-		><svg
-			width="20"
-			height="20"
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 20 20"
-			fill="currentColor"
-			style={isOpen ? 'transform: rotate(180deg)' : ''}
-		>
-			<path
-				fill-rule="evenodd"
-				d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-				clip-rule="evenodd"
-			/>
-		</svg>
+    <button on:click={toggle} aria-expanded={isOpen} id="released_button"
+        ><svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            style={isOpen ? 'transform: rotate(180deg)' : ''}
+        >
+            <path
+                fill-rule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clip-rule="evenodd"
+            />
+        </svg>
 
-		Released
-	</button>
-	{#if isOpen}
-		<div id="old_releases" transition:slide={{ duration: 300 }}>
-			{#each releases[0] as release}
-				<Movie
-					bind:title={release.Title}
-					bind:date={release.ReleaseDate}
-					bind:length={release.FilmLength}
-					bind:poster={release.Poster}
-					bind:tv={release.TV}
-					bind:disneyplus={release.Link}
-				/>
-			{/each}
-		</div>
-	{/if}
-	<div id="new_releases">
-		{#each releases[1] as release}
-			<Movie
-				bind:title={release.Title}
-				bind:date={release.ReleaseDate}
-				bind:length={release.FilmLength}
-				bind:poster={release.Poster}
-				bind:tv={release.TV}
-				bind:disneyplus={release.Link}
-			/>
-		{/each}
-	</div>
+        Recently Released
+    </button>
+    {#if isOpen}
+        <div id="old_releases" transition:slide={{ duration: 300 }}>
+            {#each releases[0] as release}
+                <Movie
+                    bind:title={release.Title}
+                    bind:date={release.ReleaseDate}
+                    bind:length={release.FilmLength}
+                    bind:poster={release.Poster}
+                    bind:tv={release.TV}
+                    bind:disneyplus={release.Link}
+                />
+            {/each}
+        </div>
+    {/if}
+    <div id="new_releases">
+        {#each releases[1] as release}
+            <Movie
+                bind:title={release.Title}
+                bind:date={release.ReleaseDate}
+                bind:length={release.FilmLength}
+                bind:poster={release.Poster}
+                bind:tv={release.TV}
+                bind:disneyplus={release.Link}
+            />
+        {/each}
+    </div>
 {:catch error}
-	<p>An error occurred:<br /> {error}</p>
+    <p>An error occurred:<br /> {error}</p>
 {/await}
 
 <style>
-	p {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		position: relative;
-		overflow: hidden;
-		font-weight: bold;
-		font-size: 3rem;
-		text-align: center;
-	}
+    p {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        overflow: hidden;
+        font-weight: bold;
+        font-size: 3rem;
+        text-align: center;
+    }
 
-	.loader {
-		width: 48px;
-		height: 48px;
-		border: 5px solid #fff;
-		border-bottom-color: transparent;
-		border-radius: 50%;
-		display: inline-block;
-		box-sizing: border-box;
-		animation: rotation 1s linear infinite;
-	}
+    .loader {
+        width: 48px;
+        height: 48px;
+        border: 5px solid #fff;
+        border-bottom-color: transparent;
+        border-radius: 50%;
+        display: inline-block;
+        box-sizing: border-box;
+        animation: rotation 1s linear infinite;
+    }
 
-	#released_button svg {
-		transition: all 0.2s;
-		margin-right: 8px;
-	}
+    #released_button svg {
+        transition: all 0.2s;
+        margin-right: 8px;
+    }
 
-	#released_button {
-		display: flex;
-		align-items: center;
-		padding-left: 15px;
-		padding-right: 15px;
-	}
+    #released_button {
+        display: flex;
+        align-items: center;
+        padding-left: 15px;
+        padding-right: 15px;
+    }
 
-	#new_releases {
-		margin-top: 40px;
-	}
+    #new_releases {
+        margin-top: 40px;
+    }
 
-	#old_releases {
-		background-color: #1f2731;
-		padding: 10px;
-		border-radius: 10px;
-		margin-bottom: 40px;
-		box-shadow: inset 0px 6px 10px rgba(0, 0, 0, 0.25);
-	}
+    #old_releases {
+        background-color: #1f2731;
+        padding: 10px;
+        border-radius: 10px;
+        margin-bottom: 40px;
+        box-shadow: inset 0px 6px 10px rgba(0, 0, 0, 0.25);
+    }
 
-	#old_releases :global(div:last-child) {
-		margin-bottom: 5px;
-	}
+    #old_releases :global(div:last-child) {
+        margin-bottom: 5px;
+    }
 
-	@keyframes rotation {
-		0% {
-			transform: rotate(0deg);
-		}
-		100% {
-			transform: rotate(360deg);
-		}
-	}
+    @keyframes rotation {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 
-	.center {
-		display: flex;
-		justify-content: center;
-	}
+    .center {
+        display: flex;
+        justify-content: center;
+    }
 </style>
